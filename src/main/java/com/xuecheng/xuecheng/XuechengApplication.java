3fc371361d2f9cd4e0bbe54d8d6e1176dfaa4868
XM <xm0108251@163.com>
2024-03-01 13:36:10 +0800

    初始

---
+++ src/main/java/com/xuecheng/xuecheng/XuechengApplication.java
@@ -0,0 +1,13 @@
+package com.xuecheng.xuecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuechengApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuechengApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xuecheng/xuecheng/XuechengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuecheng.xuecheng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuechengApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
